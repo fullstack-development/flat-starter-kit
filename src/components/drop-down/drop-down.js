7ad866d3b9db91750f876228b66aa0c0613e128a
Dmitriy Serdtsev <in19farkt@gmail.com>
2017-01-06 17:13:57 +0700

    Rewrite component drop-down. Use jquery-ui selectmenu.

--- src/components/drop-down/drop-down.js
+++ src/components/drop-down/drop-down.js
@@ -1,1 +1,23 @@
 import './drop-down.styl';
+
+if (!$.fn.selectmenu) {
+  require('jquery-ui/ui/widgets/selectmenu');
+  require('jquery-ui/themes/base/selectmenu.css');
+}
+
+class DropDown {
+  constructor($component) {
+    this.$component = $component;
+    this.render();
+  }
+
+  render() {
+    $('.js-drop-down__select-box', this.$component).selectmenu();
+  }
+}
+
+$(() => {
+  $('.drop-down').each((index, node) => {
+    new DropDown($(node));
+  });
+});
