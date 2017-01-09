c5c385cd51b498958028a22dc06ad085fe73095f
Dmitriy Serdtsev <in19farkt@gmail.com>
2017-01-09 16:04:19 +0700

    Refactoring form elements

--- src/components/tick-box/tick-box.js
+++ src/components/tick-box/tick-box.js
@@ -1,1 +1,19 @@
+import $ from 'jquery';
 import './tick-box.styl';
+
+class TickBox {
+  constructor($component) {
+    this.$component = $component;
+    this._attachEventHandlers();
+  }
+
+  _attachEventHandlers() {
+    this.$component.on('click', () => this.$component.toggleClass('tick-box_enable'))
+  }
+}
+
+$(() => {
+  $('.js-tick-box').each((index, node) => {
+    new TickBox($(node));
+  });
+});
