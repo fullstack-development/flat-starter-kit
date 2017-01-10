4afb505235c96c6d2bcc0d3cefcf74e35619f1f1
Dmitriy Serdtsev <in19farkt@gmail.com>
2017-01-10 14:23:52 +0700

    Finished work on header and menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,1 +1,20 @@
-import './menu.styl'
+import './menu.styl';
+import $ from 'jquery';
+
+class Menu {
+  constructor($component) {
+    this.$component = $component;
+    this._attachEventHandler();
+    this.$list = $('.js-menu__list', this.$component);
+  }
+
+  _attachEventHandler() {
+    $('.js-menu__button', this.$component).on('click', () => this.$list.toggleClass('menu__list_open'));
+  }
+}
+
+$(() => {
+  $('.js-menu').each((index, node) => {
+    new Menu($(node));
+  });
+});
