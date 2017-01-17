0add37cc0d28bc7e1edf0f4c8e20f5d005a491ea
Dmitriy Serdtsev <in19farkt@gmail.com>
2017-01-17 12:00:44 +0700

    Line overflow control in description for news and events components

--- src/components/news/news.js
+++ src/components/news/news.js
@@ -1,1 +1,37 @@
 import './news.styl';
+import $ from 'jquery';
+
+class News {
+  constructor($component) {
+    this.$component = $component;
+    this.$description = $('.js-news__description', this.$component);
+    this._setColumnWidth();
+    this._attachEventHandlers();
+  }
+
+  _attachEventHandlers() {
+    $(window).resize(() => this._setColumnWidth());
+  }
+
+  _setColumnWidth() {
+    const width = this.$description.width();
+    const height = this.$description.height();
+    this.$description
+      .css({
+        '-webkit-column-width': `${width}px`,
+        '-moz-column-width': `${width}px`,
+        'column-width': `${width}px`,
+        'height': `${height}px`,
+      });
+  }
+}
+
+export default function renderComponent() {
+  $(() => {
+    $('.js-news').each((index, node) => {
+      new News($(node));
+    });
+  });
+}
+
+renderComponent();
